5f6ac4e5e3be1409095212fc9d1ea9c1ff063e46
胥涛 <taoxu@iflytek.com>
2020-03-23 16:32:28 +0800

    chore: init project

---
+++ webpack.config.build.js
@@ -0,0 +1,82 @@
+let path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "./"),
+    filename: "index.js",
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-typescript',
+              [
+                '@babel/preset-env',
+                {
+                  "useBuiltIns": "usage",
+                  "corejs": '3.3'
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        oneOf: [{
+          resourceQuery: /^\?raw$/,
+          use: [
+            require.resolve('to-string-loader'),
+            require.resolve('css-loader'),
+            require.resolve('postcss-loader')
+          ]
+        }, {
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  mode: 'local',
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                  context: path.resolve(__dirname, 'src'),
+                  hashPrefix: 'my-custom-hash',
+                }
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ]
+        }]
+      },
+      {
+        test: /\.(png|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+let path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  entry: {
+    'index': [path.resolve(__dirname, "./src/index.ts")],
+    // 'pinao-roll': [path.resolve(__dirname, "./src/components/PianoRoll/PianoRoll")]
+  },
+  output: {
+    path: path.resolve(__dirname, "./"),
+    filename: "[name].js",
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./"),
+    compress: true,
+    port: 9000,
+    host: '127.0.0.1'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-typescript',
+              [
+                '@babel/preset-env',
+                {
+                  "useBuiltIns": "usage",
+                  "corejs": '3.3'
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        oneOf: [{
+          resourceQuery: /^\?raw$/,
+          use: [
+            require.resolve('to-string-loader'),
+            require.resolve('css-loader'),
+            require.resolve('postcss-loader')
+          ]
+        }, {
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  mode: 'local',
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                  context: path.resolve(__dirname, 'src'),
+                  hashPrefix: 'my-custom-hash',
+                }
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ]
+        }]
+      },
+      {
+        test: /\.(png|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, "./dev.html")
+    })
+  ]
+}
